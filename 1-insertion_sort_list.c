3c2543445ade2b1be19029a85aa00807efde473b
sethgreen23 <dridi.chaith@gmail.com>
2023-12-15 12:38:50 -0800

    another  change

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -68,7 +68,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *previous = NULL, *current = NULL, *pointer = NULL;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 	pointer = *list;
 	while (pointer != NULL)
