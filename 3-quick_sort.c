a103b09fa71f15d6b2d5f64decd21b01b56d54d2
sethgreen23 <dridi.chaith@gmail.com>
2023-12-16 06:51:37 -0800

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,20 +26,26 @@ void swap(int *a, int *b)
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low - 1;
+	int i = low;
 	int j;
 
 	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
-			i++;
-			swap(&array[i], &array[j]);
-			print_array(array, size);
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+				i++;
+			}
 		}
 	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
+	if ((i + 1) != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
 	return (i + 1);
 }
 
